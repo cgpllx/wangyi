950fb1c467a367e8247fff2f4293021e540f6a2c
cgpllx <cgpllx1@qq.com>
2014-08-29 20:52:15 +0800

    Signed-off-by: cgpllx <cgpllx1@qq.com>

---
+++ src/com/kubeiwu/wangyi/MainActivity.java
@@ -0,0 +1,15 @@
+package com.kubeiwu.wangyi;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+//		getSupportFragmentManager().beginTransaction().replace(R.id.fragment, new SampleTabsStyled());
+	}
+
+}
