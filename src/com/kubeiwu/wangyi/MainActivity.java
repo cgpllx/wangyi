d2c4a2992b216a89e4bdf5f24e9d33a82e1bffbe
cgpllx <cgpllx1@qq.com>
2014-08-29 21:01:20 +0800

    Signed-off-by: cgpllx <cgpllx1@qq.com>

--- src/com/kubeiwu/wangyi/MainActivity.java
+++ src/com/kubeiwu/wangyi/MainActivity.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 
 public class MainActivity extends ActionBarActivity {
-
+//	https://github.com/cgpllx/wangyi.git
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
